7c8295b56121d5a239b0a540f82842fc68ef04b3
Evan Wong <evan.wong1@ucalgary.ca>
2022-04-12 11:58:22 -0600

    changes

--- login.php
+++ login.php
@@ -1,30 +1,53 @@
 <?php
-// database connection code
-if(isset($_POST['txtName']))
-{
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','shardarq_db_connect');
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'shardarq_db_connect';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+if ( !isset($_POST['txtName'], $_POST['txtPassword']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
 
 // get the post records
 
-$txtName = $_POST['txtName'];
-//$txtEmail = $_POST['txtEmail'];
-$txtPassword = $_POST['txtPassword'];
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['txtName']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+	if ($stmt->num_rows > 0) {
+		$stmt->bind_result($id, $fldPassword);
+		$stmt->fetch();
+		// Account exists, now we verify the password.
+		// Note: remember to use password_hash in your registration file to store the hashed passwords.
+		if (password_verify($_POST['txtPassword'], $fldPassword)) {
+			// Verification success! User has logged-in!
+			// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+			session_regenerate_id();
+			$_SESSION['loggedin'] = TRUE;
+			$_SESSION['name'] = $_POST['txtName'];
+			$_SESSION['id'] = $id;
+			echo 'Welcome ' . $_SESSION['name'] . '!';
+		} else {
+			// Incorrect password
+			echo 'Incorrect username and/or password!';
+		}
+	} else {
+		// Incorrect username
+		echo 'Incorrect username and/or password!';
+	}
 
-
-// database insert SQL code
-$sql = "INSERT INTO `tbl_login` (`Id`, `fldUsername`, `fldPassword`) VALUES ('0', '$txtName', '$txtPassword')";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-if($rs)
-{
-	echo "Username inserted";
-}
-}
-else
-{
-	echo "Password inserted";
-	
+	$stmt->close();
 }
 ?>
