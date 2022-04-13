299493af4e36ef6be62e597da0bee70b3333eef5
Evan Wong <evan.wong1@ucalgary.ca>
2022-04-13 14:13:32 -0600

    changes in userdash.php

--- userdash.php
+++ userdash.php
@@ -2,7 +2,7 @@
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 
-include('userdash.html');
+readfile('userdash.html');
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: cpscproj.html');
