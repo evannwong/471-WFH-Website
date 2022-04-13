20f6b405effdfe6b802e8537822e074ea09cac26
Evan Wong <evan.wong1@ucalgary.ca>
2022-04-12 23:44:23 -0600

    php stuff

--- userdash.php
+++ userdash.php
@@ -1,13 +1,11 @@
 <?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
+
+include('userdash.html');
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: cpscproj.html');
-	exit;
-}
-if(isset($_SESSION['loggedin'])){
-	header('Location: userdash.html');
 	exit;
 }
 ?>
